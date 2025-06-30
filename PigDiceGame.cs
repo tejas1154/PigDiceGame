292ec88d9edcb44910247a83fc86bf57ef67c09c
tejas1154 <tejas.ramane@aurionpro.com>
2025-06-30 08:55:53 +0530

    Add files via upload

---
+++ PigDiceGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDiceGame
+{
+    internal class PigDiceGame
+    {
+        public static int turnScore = 0;
+        public static int finalScore = 0;
+        static int totalTurns = 1;
+        public static void startGame()
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to my game");
+            Console.WriteLine("Set the target");
+            int target = int.Parse(Console.ReadLine());
+
+            while(finalScore < target)
+            {
+                Console.WriteLine("\nTurn = " + totalTurns);
+                char userChoice = 'r';
+                turnScore = 0;
+
+                while(userChoice == 'r')
+                {
+                    Console.WriteLine("Enter 'r' for role or 'h' for hold");
+                    userChoice = char.Parse(Console.ReadLine());
+
+                    if (userChoice == 'h') break;
+
+                    int diceScore = random.Next(1, 10);
+
+                    if(diceScore == 1)
+                    {
+                        Console.WriteLine("Current Dice is = " + diceScore);
+                        Console.WriteLine("Oops.. you got 1.. No Score is added");
+                        turnScore = 0;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Current Dice is = " + diceScore);
+                        turnScore += diceScore;
+                    }      
+                }
+
+                finalScore += turnScore;
+
+                Console.WriteLine("\nTotal Turn = " + totalTurns);
+                Console.WriteLine("Turn Score = " + turnScore);
+                Console.WriteLine("Final Score = " + finalScore);
+
+                if(finalScore >= target)
+                {
+                    Console.WriteLine("\nYou Won!!!!!!!!!!!!!!!!");
+                    Console.WriteLine("Total Turns taken = " + totalTurns);
+                }
+                totalTurns++;
+            }
+        }
+    }
+}
